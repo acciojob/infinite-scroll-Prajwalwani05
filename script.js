8281840ec4ace9ac8df9381ac20298c622d6e879
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-06-19 19:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //your code here!
+// Get the list element
+const list = document.querySelector('infi-list');
+
+// Add 10 list items by default
+for (let i = 1; i <= 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = `List Item ${i}`;
+  list.appendChild(listItem);
+}
+
+// Function to add 2 more list items automatically
+function addMoreItems() {
+  for (let i = 1; i <= 2; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = `List Item ${list.children.length + i}`;
+    list.appendChild(listItem);
+  }
+}
+
+// Event listener to check if user has reached the end of the list
+list.addEventListener('scroll', function() {
+  const scrollTop = list.scrollTop;
+  const scrollHeight = list.scrollHeight;
+  const clientHeight = list.clientHeight;
+
+  if (scrollTop + clientHeight >= scrollHeight) {
+    addMoreItems();
+  }
+});
+
+
 
